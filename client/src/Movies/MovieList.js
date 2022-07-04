60e5f637d0f9d133e7c0fec2303ce3742ea12945
Uchechi Nwabia <nwabiaesther@gmail.com>
2022-07-04 18:52:21 +0100

    sol

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link key={movie.id} to= {`./movies/${movie.id}`}><MovieDetails  movie={movie} /></Link>
       ))}
     </div>
   );
